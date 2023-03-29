1b2bc7a04ef4fdb96b353db409277988ab67bf01
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-03-29 12:39:53 +0000

    is root 2.0

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL && ( node->left == NULL || node->right == NULL))
-		return (0);
-	else
+	if (node != NULL && node->parent == NULL)
 		return (1);
+	else
+		return (0);
 }
